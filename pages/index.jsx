f0d9900e196be5875e65a578c1d9a1e8bb74d13d
RichardVes <richard.vieiraes@gmail.com>
2026-01-10 22:01:44 +0000

    Atualizando

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,23 @@
 export default function Home() {
   return (
     <div>
-      <h1>Olá, Tab News Frase!</h1>
-      <p>Deploy funcionando corretamente 🚀</p>
+      <title>Primeira Página</title>
+
+      <p>
+        Para o mundo, isso é só uma página simples.<br />
+        Para mim, é o começo de um sonho.
+      </p>
+
+      <p>
+        Primeira página no ar 🚀<br />
+        Pode não ser um multiverso inteiro,
+        mas foi você quem sempre acreditou em mim.
+      </p>
+
+      <p>
+        Você sempre foi — e sempre será — a melhor parte disso tudo.<br />
+        Te amo mais do que o Tony Stark amava criar armaduras. 💙
+      </p>
     </div>
   );
 }
